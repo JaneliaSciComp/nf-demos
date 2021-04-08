cef6dd9a0cff78d8489863a189d030a392ff224c
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-04-07 22:26:08 -0400

    implemented n5 metadata

--- containers/n5-tools-py/scripts/n5_multiscale.py
+++ containers/n5-tools-py/scripts/n5_multiscale.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python
 
 import argparse
+import math
 import numpy as np 
-import zarr 
 import dask.array as da
+import zarr 
+from zarr.errors import PathNotFoundError
 from xarray_multiscale import multiscale
 
+def add_metadata(n5_path, downsampling_factors=(2,2,2), axes=("x","y","z"), pixel_res=None, pixel_res_units="nm"):
+    store = zarr.N5Store(n5_path)
+    scales = []
 
-def add_multiscale(input_path, data_set, downsampling_factors=(2,2,2), \
+    for idx in range(20):
+        try:
+            zarr.open(store, path=f'/s{idx}', mode='r')
+        except PathNotFoundError:
+            break
+        factors = tuple([int(math.pow(f,idx)) for f in downsampling_factors])
+        print(f"Setting downsampling factors for scale {idx} to {factors}")
+        scales.append(factors)
+        if idx>0:
+            z = zarr.open(store, path=f'/s{idx}', mode='a')
+            z.attrs["downsamplingFactors"] = factors
+
+    z = zarr.open(store, path=f'/', mode='a')
+    z.attrs["scales"] = scales
+    if axes:
+        z.attrs["axes"] = axes
+    if pixel_res:
+        z.attrs["pixelResolution"] = {
+            "dimensions": pixel_res,
+            "unit": pixel_res_units
+        }
+    print("Added multiscale metadata to", n5_path)
+
+
+def add_multiscale(n5_path, data_set, downsampling_factors=(2,2,2), \
         downsampling_method=np.mean, thumbnail_size_yx=None):
     '''
     Given an n5 with "s0", generate downsampled versions s1, s2, etc., up to the point where
     the smallest version is larger than thumbnail_size_yx (which defaults to the chunk size).
     '''
-    print('Generating multiscale for', input_path)
-    store = zarr.N5Store(input_path)
+    print('Generating multiscale for', n5_path)
+    store = zarr.N5Store(n5_path)
     volume = da.from_zarr(store, component=f'{data_set}/s0')
     print(volume)
     chunk_size = volume.chunksize
@@ -29,9 +58,9 @@ def add_multiscale(input_path, data_set, downsampling_factors=(2,2,2), \
         print(f'Saving level {idx}')
         m.data.to_zarr(store, component=f'{data_set}/s{idx}', overwrite=True)
         z = zarr.open(store, path=f'{data_set}/s{idx}', mode='a')
-        z.attrs["downsamplingFactors"] = np.power(downsampling_factors, idx)
-        
-    print("Added multiscale to", input_path)
+        z.attrs["downsamplingFactors"] = tuple([int(math.pow(f,idx)) for f in downsampling_factors])
+
+    print("Added multiscale imagery to", n5_path)
 
 
 def main():
@@ -46,6 +75,12 @@ def main():
     parser.add_argument('-f', '--downsampling_factors', dest='downsampling_factors', type=str, default="2,2,2", \
         help='Downsampling factors for each dimension (default "2,2,2")')
 
+    parser.add_argument('-p', '--pixel_res', dest='pixel_res', type=str, \
+        help='Pixel resolution for each dimension "2.0,2.0,2.0" (default None) - required for Neuroglancer')
+
+    parser.add_argument('-u', '--pixel_res_units', dest='pixel_res_units', type=str, default="nm", \
+        help='Measurement unit for --pixel_res (default "nm") - required for Neuroglancer')
+
     parser.add_argument('--distributed', dest='distributed', action='store_true', \
         help='Run with distributed scheduler (default)')
     parser.set_defaults(distributed=False)
@@ -56,6 +91,10 @@ def main():
     parser.add_argument('--dashboard', dest='dashboard', action='store_true', \
         help='Run a web-based dashboard on port 8787')
     parser.set_defaults(dashboard=False)
+
+    parser.add_argument('--metadata-only', dest='metadata_only', action='store_true', \
+        help='Only fix metadata on an existing multiscale pyramid')
+    parser.set_defaults(metadata_only=False)
 
     args = parser.parse_args()
 
@@ -75,8 +114,16 @@ def main():
         pbar = ProgressBar()
         pbar.register()
 
-    add_multiscale(args.input_path, args.data_set, \
-        downsampling_factors=[int(c) for c in args.downsampling_factors.split(',')])
+    downsampling_factors = [int(c) for c in args.downsampling_factors.split(',')]
+
+    pixel_res = None
+    if args.pixel_res:
+        pixel_res = [float(c) for c in args.pixel_res.split(',')]
+
+    if not args.metadata_only:
+        add_multiscale(args.input_path, args.data_set, downsampling_factors=downsampling_factors)
+
+    add_metadata(args.input_path, downsampling_factors=downsampling_factors, pixel_res=pixel_res, pixel_res_units=args.pixel_res_units)
 
 
 if __name__ == "__main__":
